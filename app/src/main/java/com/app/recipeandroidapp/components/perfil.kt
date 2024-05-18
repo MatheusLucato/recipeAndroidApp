4ccccc148b5fbf7c393f0bb7b098c4cb55b852a3
Micael Fone <micaelfone0@gmail.com>
2024-05-18 17:53:03 -0300

    Adicionando telas de cadastro e login

---
+++ app/src/main/java/com/app/recipeandroidapp/components/perfil.kt
@@ -0,0 +1,14 @@
+package com.app.recipeandroidapp.components
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.recipeandroidapp.R
+
+class perfil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_perfil)
+        supportActionBar?.hide() // Melhor usar o operador safe call
+    }
+}
